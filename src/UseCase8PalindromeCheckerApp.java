df62c0fdab167d8b5f4698753a4aeb0cfb4fdfc0
ashutosh2453 <ashutosh.c@icloud.com>
2026-02-25 18:46:46 +0530

    UC8: Linked list based palindrome checker

---
+++ src/UseCase8PalindromeCheckerApp.java
@@ -0,0 +1,91 @@
+public class UseCase8PalindromeCheckerApp {
+
+    // Node definition for Singly Linked List
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        // Convert string to linked list
+        Node head = createLinkedList(input);
+
+        // Check palindrome
+        if (isPalindrome(head)) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+
+    // Create linked list from string
+    static Node createLinkedList(String str) {
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < str.length(); i++) {
+            Node newNode = new Node(str.charAt(i));
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+
+    // Palindrome check using linked list
+    static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // Fast & Slow pointer to find middle
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node secondHalf = reverseList(slow);
+        Node firstHalf = head;
+
+        // Compare both halves
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    // Reverse linked list (in-place)
+    static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+}
